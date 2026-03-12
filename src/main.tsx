b229a20e5eae0fafecde2a3fedab8a311f992061
Joash Adeoye <>
2026-03-12 01:41:00 +0100

    Debug: Direct render in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+console.log('MAIN SCRIPT EXECUTING');
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={{ padding: '100px', background: 'blue', color: 'white', size: '50px' }}>
+    REACT MOUNTED CORE SUCCESS!!
+  </div>
 )
+
